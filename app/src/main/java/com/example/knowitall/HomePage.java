e4b96cd9111716ed0b60c35f4526888d3b7e71f6
QuangMinh5703 <minhtinhtinh57@gmail.com>
2024-06-25 23:35:18 +0700

    update onclick changepassword

--- app/src/main/java/com/example/knowitall/HomePage.java
+++ app/src/main/java/com/example/knowitall/HomePage.java
@@ -4,14 +4,8 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-<<<<<<< HEAD
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-=======
->>>>>>> origin/master
 
 public class HomePage extends AppCompatActivity {
 
@@ -20,18 +14,8 @@ public class HomePage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home_page);
-<<<<<<< HEAD
         RecyclerView recyclerView = findViewById(R.id.recyclerView_prj);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         };
     }
-=======
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-    }
-}
->>>>>>> origin/master
